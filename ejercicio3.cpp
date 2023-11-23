49f25cfc5a33580ef011e5a96da271c0851ebe3a
sofivasa <sofivasa@myuax.com>
2023-11-23 12:57:06 +0100

    comentarios

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -2,10 +2,15 @@
 #include <string>
 
 int main () {
+    //Crear cadena
     std:: string cadena = "Ejemplo 3";
+    //Imprimir cadena
     std:: string subcadena_buscar= "3";
+    //Buscar cadena
     size_t indice_inicio= cadena.find(subcadena_buscar);
+    // Imprimir cadena
     if (indice_inicio != std::string::npos){
+
         std:: cout<< "La subcadena"<< subcadena_buscar<< "se encuentra en la posicion"<<indice_inicio<<std ::endl;
 
         std:: string nueva_subcadena= "tres";
