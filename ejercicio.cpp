5835ae4546c96fe485dbd66dc53c205536eec595
sofivasa <sofivasa@myuax.com>
2023-11-23 12:41:30 +0100

    comentarios ejercicio1

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -3,7 +3,9 @@
 
 
 int main (){
+    //Crear cadena STL
     std:: string phrase =" Este es un ejemplo de cadenas STL ";
+    //Imprimir la cadena
     std::cout << "La frase es:"<< phrase << std :: endl;
     return 0;
 }
